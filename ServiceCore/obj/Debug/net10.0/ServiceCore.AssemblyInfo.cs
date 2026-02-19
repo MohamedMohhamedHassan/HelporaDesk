cdbdc894cdb7fb441143dc0edad4a1f0290204ab
MohamedMohhamedHassan <imohamedhassan999@hotmail.com>
2026-02-19 14:47:30 +0200

    build: Add generated build artifacts for the ServiceCore project.

--- ServiceCore/obj/Debug/net10.0/ServiceCore.AssemblyInfo.cs
+++ ServiceCore/obj/Debug/net10.0/ServiceCore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ServiceCore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54b3e46c29a468f9f60ea6967523526c5528a833")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6820847101341ba3579ffad49f89c42148e3e6f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ServiceCore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ServiceCore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
